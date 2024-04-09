3c7b6c787d7c3a444b6c29f1633c3999a306b96f
LittleCharles <luis.carlos.vieira@live.com>
2024-04-09 18:51:14 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+import { Login } from "./pages/Login"
+
+function App() {
+
+
+  return (
+    <>
+     <Login/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Login.jsx
@@ -0,0 +1,55 @@
+import { Button } from "../components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export function Login(){
+    return (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-300">
+          <div className="md:max-w-[70%] w-full bg-primary rounded-[40px] flex flex-col rounded-br-[80px] relative">
+            <div className="flex-1 flex flex-col md:flex-row h-full">
+              <div className="flex-1 md:w-1/2 flex flex-col gap-3 p-5 items-center justify-center">
+                <p className="text-white text-8xl">LOGO</p>
+                
+              </div>
+              <form className="flex-1 flex flex-col gap-3 p-5 bg-white rounded-tr-[40px] rounded-br-[80px] rounded-tl-[80px] justify-center">
+                <div className="flex flex-col items-center px-5 py-8 w-full">
+                  <span className="text-bg-primary text-4xl text-center font-normal">
+                    Olá, <br /> Seja bem vindo!
+                  </span>
+                  <div className="flex flex-col gap-8 w-full mt-10">
+                    <Input
+                      placeholder="E-mail"
+                      
+                    />
+                    <Input
+                      placeholder="Senha"
+                      type="password"
+                     
+                    />
+                  </div>
+                  <span
+                    className="text-bg-primary text-base cursor-pointer underline text-end w-full mt-2"
+                   
+                  >
+                    Esqueceu a senha?
+                  </span>
+                  <Button
+                    variant="default"
+                    type="button"
+                    className="bg-primary text-white text-lg w-full rounded-[5px] h-14 mt-10"
+                    
+                  >
+                   Fazer Login
+                  </Button>
+                  <span
+                    className="text-bg-primary text-base cursor-pointer underline text-center w-full mt-2"
+                    
+                  >
+                    Criar conta
+                  </span>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      );
+    }
