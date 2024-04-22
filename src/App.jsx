52c2c129de068b50d3b604b0fb71facff0edefa7
Thiago Faro <thfaro.ads@gmail.com>
2024-04-22 14:38:23 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { Button } from "./components/ui/button"
-import { Login } from "./pages/Login"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Register } from "./pages/Register"
+import { Button } from "./components/ui/button";
+import { Login } from "./pages/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Register } from "./pages/Register";
+import { ForgotPassword } from "./pages/ForgotPassword"; 
 
 function App() {
-
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login/>}/>
-      <Route path="/cadastro" element={<Register/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<Register />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} /> 
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
