cbe93fc17d77d8a94d3b11258ac55cb10c4a1d5f
LittleCharles <luis.carlos.vieira@live.com>
2024-05-28 19:12:17 -0300

    fix search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -14,6 +14,7 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { clothes } from '@/mock/cardProducts';
+import Footer from '@/components/Footer';
 
 export function Search() {
   const navigate = useNavigate();
@@ -31,35 +32,45 @@ export function Search() {
   return (
     <>
       <Header />
-      <div className="m-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10">
-          {filteredData.map((searchCloth) => (
-            <div key={searchCloth.id} className="min-w-[250px] max-w-[250px] border-primary border rounded-lg">
-              <Card className="rounded-none border-none bg-card-none shadow-none">
-                <CardContent className="p-0">
-                  <div className="relative">
-                    <div className="absolute top-3 left-3 z-50">
-                      <Badge variant="secondary" className="opacity-90 flex gap-1 items-center top-3 left-3">
-                        <Heart size={22} className="text-primary" />
-                      </Badge>
+      <div className="mt-16 mb-16 flex flex-col max-w-[1400px] m-auto">
+        <h1 className="text-4xl font-semibold mb-10 text-primary">
+          Resultado de pesquisa por: {query}
+        </h1>
+        {filteredData.length > 0 ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10">
+            {filteredData.map((searchCloth) => (
+              <div key={searchCloth.id} className="min-w-[250px] max-w-[250px] border-primary border rounded-lg">
+                <Card className="rounded-none border-none bg-card-none shadow-none">
+                  <CardContent className="p-0">
+                    <div className="relative">
+                      <div className="absolute top-3 left-3 z-50">
+                        <Badge variant="secondary" className="opacity-90 flex gap-1 items-center top-3 left-3">
+                          <Heart size={22} className="text-primary" />
+                        </Badge>
+                      </div>
+                      <img className="w-full h-[250px] rounded-t-lg" src={searchCloth.image} alt={searchCloth.name} />
                     </div>
-                    <img className="w-full h-[250px] rounded-t-lg" src={searchCloth.image} alt={searchCloth.name} />
-                  </div>
-                  <div className="flex flex-col gap-2 p-4">
-                    <h3>{searchCloth.name}</h3>
-                    <p>{searchCloth.size}</p>
-                    <Button className="bg-white text-primary border border-primary">Ver mais</Button>
-                    <Button className="text-white hover:bg-white hover:text-primary border border-primary">Enviar proposta</Button>
-                  </div>
-                </CardContent>
-              </Card>
-            </div>
-          ))}
+                    <div className="flex flex-col gap-2 p-4">
+                      <h3>{searchCloth.name}</h3>
+                      <p>{searchCloth.size}</p>
+                      <Button className="bg-white text-primary border border-primary">Ver mais</Button>
+                      <Button className="text-white hover:bg-white hover:text-primary border border-primary">Enviar proposta</Button>
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p className="text-center text-gray-500 text-xl">Nenhum produto encontrado</p>
+        )}
+        <div className='flex justify-center'>
+          <Button className="mt-10 text-white hover:bg-white hover:text-primary border border-primary w-[250px]" onClick={returnHome}>
+            Voltar ao início
+          </Button>
         </div>
       </div>
-      <div>
-        <center><Button className="margin-top:30px  text-white hover:bg-white hover:text-primary border border-primary" onClick={returnHome}>Voltar ao início</Button></center>
-      </div>
+      <Footer/>
     </>
   );
 };
