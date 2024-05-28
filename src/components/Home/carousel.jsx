f69d3dc09dde3b7f91dd2b8161aca6c88dcbb689
LittleCharles <luis.carlos.vieira@live.com>
2024-05-28 19:28:39 -0300

    merge LC into main

--- src/components/Home/carousel.jsx
+++ src/components/Home/carousel.jsx
@@ -1,26 +1,21 @@
-import * as React from "react"
+import * as React from "react";
 
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
-import image1 from '../../assets/logo banner.png';
+} from "@/components/ui/carousel";
+import image1 from "../../assets/logo banner.png";
 import image2 from "../../assets/loja banner.png";
-import image3 from '../../assets/roupas inverno.png';
+import image3 from "../../assets/roupas inverno.png";
 
-const imageUrls = [
-  image1,
-  image2,
-  image3,
-];
+const imageUrls = [image1, image2, image3];
 
 export default function CarouselHome() {
   return (
-  
     <Carousel className="w-full mt-16">
       <CarouselContent>
         {imageUrls.map((url, index) => (
@@ -28,7 +23,11 @@ export default function CarouselHome() {
             <div>
               <Card className="h-[500px]">
                 <CardContent className="flex aspect-square items-center justify-center p-0 h-[500px] w-full">
-                  <img className="rounded-lg" src={url} alt={`Imagem ${index + 1}`} />
+                  <img
+                    className="rounded-lg"
+                    src={url}
+                    alt={`Imagem ${index + 1}`}
+                  />
                 </CardContent>
               </Card>
             </div>
@@ -38,5 +37,5 @@ export default function CarouselHome() {
       <CarouselPrevious />
       <CarouselNext />
     </Carousel>
-  )
+  );
 }
